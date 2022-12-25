2bb68cdd17bbbe9277199a32ee82ceb521189767
Delarram <tunyanaing960@gmail.com>
2022-12-25 19:12:43 +0630

    complete landing page

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -108,7 +108,120 @@ class _LandingPageState extends State<LandingPage> {
             const SizedBox(
               height: 12,
             ),
-
+            if (currentPage == 2)
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CustomButton(
+                          nextPage: () {
+                            Navigator.of(context).pushAndRemoveUntil(
+                                MaterialPageRoute(
+                                    builder: (context) =>  Container()
+                                ),
+                                    (route) => false);
+                          },
+                          text: 'LogIn',
+                          height: 50,
+                          width: double.infinity,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: CustomButton(
+                          nextPage: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (_)=>Container()));
+                          },
+                          text: 'SignIn',
+                          height: 50,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            else
+              Padding(
+                padding:
+                const EdgeInsets.only(bottom: 30, left: 20, right: 20),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          if (currentPage == 0) {
+                            currentPage = 2;
+                          } else {
+                            currentPage += 1;
+                          }
+                          pageController.animateToPage(currentPage,
+                              curve: Curves.decelerate,
+                              duration: const Duration(milliseconds: 300));
+                        });
+                      },
+                      child:
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const CustomTextView(
+                            text: "Skip",
+                            fontWeight: FontWeight.bold,
+                          ),
+                          const SizedBox(
+                            height: 2,
+                          ),
+                          Container(
+                            height: 1.h,
+                            width: 15.w,
+                            color: Colors.black,
+                          ),
+                        ],
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (currentPage == 2) {
+                            currentPage = 0;
+                          } else {
+                            currentPage += 1;
+                          }
+                          pageController.animateToPage(currentPage,
+                              curve: Curves.decelerate,
+                              duration: const Duration(milliseconds: 300));
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.transparent,
+                            border: Border.all(
+                                width: 1, color: const Color(0xff31347A))),
+                        padding: const EdgeInsets.all(3),
+                        child: Container(
+                          height: 25,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: const Color(0xff31347A),
+                          ),
+                          child: const Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                            size: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            SizedBox(height: 20.h,)
           ],
         ),
       ),
