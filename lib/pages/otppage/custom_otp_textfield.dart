17983fd2c0ebd95ce4d1224ffd28c2c521f285b6
Delarram <tunyanaing960@gmail.com>
2023-01-03 16:22:51 +0630

    implemented textfield

--- lib/pages/otppage/custom_otp_textfield.dart
+++ lib/pages/otppage/custom_otp_textfield.dart
@@ -1,9 +1,5 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-
 
 class CustomOtpTextField extends StatelessWidget {
   final double height;
