751b56638c2d1fe11e97580a9750ba88edd80a63
Delarram <tunyanaing960@gmail.com>
2023-01-05 11:31:49 +0630

    showloadingdialog

---
+++ lib/widget/loading_indicator.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/widget/custom_text.dart';
+
+
+void showLoadingIndicator(BuildContext context) {
+  AlertDialog alert = AlertDialog(
+    content: Column(
+      children: [
+        CircularProgressIndicator(
+        ),
+        Container(
+            margin: const EdgeInsets.only(left: 7),
+            child: CustomTextView(text: "Please wait",)),
+      ],
+    ),
+  );
+  showDialog(
+    barrierDismissible: false,
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
