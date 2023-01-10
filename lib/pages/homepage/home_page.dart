e596e5534050ac742eba70c484698d70fbfb6bb1
Delarram <tunyanaing960@gmail.com>
2023-01-10 23:58:21 +0630

    added course listview

--- lib/pages/homepage/home_page.dart
+++ lib/pages/homepage/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:untitled/pages/homepage/homepageviewitem/welcome_section.dart';
 import 'package:untitled/widget/custom_text.dart';
-import 'homepageviewitem/appbar_section.dart';
 import 'homepageviewitem/statusbar_section.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 class HomePage extends StatelessWidget {
@@ -14,66 +12,7 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-           Stack(
-             children: [
-               StatusBarSection(),
-               SizedBox(
-                 height: 300,
-                 child: ListView.separated(
-                   itemCount: 5,
-                   shrinkWrap: true,
-                   scrollDirection: Axis.horizontal,
-                   separatorBuilder: (BuildContext context, int index) => SizedBox(width: 10,),
-                   itemBuilder: (BuildContext context, int index) {
-                     return Column(
-                       crossAxisAlignment: CrossAxisAlignment.start,
-                       children: [
-                         Padding(
-                           padding: const EdgeInsets.symmetric(horizontal: 12),
-                           child: Row(
-                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                             children: [
-                               CustomTextView(text: "Beginner Course",fontSize: 24.sp,fontWeight: FontWeight.w500,),
-                               CustomTextView(text: "See all",fontSize: 14.sp,fontWeight: FontWeight.w500,fontColor: Colors.grey.shade600,),
-                             ],
-                           ),
-                         ),
-                         ClipRRect(
-                             borderRadius: BorderRadius.circular(15),
-                             child: CachedNetworkImage(
-                               height: 200,
-                               width: 200,
-                               fit: BoxFit.fill,
-                               imageUrl: "https://images.squarespace-cdn.com/content/v1/5a0d6e5dcd39c35d0c651639/1561378813145-POA9V823FOBYVV7RPH4K/5+Beginner+ESL+Lesson+Plans+You+Need+to+Try?format=500w",
-                               errorWidget: (
-                                   context,
-                                   url,
-                                   error,
-                                   ) => const Icon(Icons.error), progressIndicatorBuilder: (context, url, downloadProgress,) =>
-                                 Center(
-                                   child: CircularProgressIndicator(
-                                       value: downloadProgress.progress),
-                                 ),
-                             )),SizedBox(height: 5,),
-                         CustomTextView(
-                           text: "Beginner Cource",
-                           fontColor: Colors.black,
-                           fontSize: 14.sp,
-                           fontWeight: FontWeight.w600,
-                         ),
-                         SizedBox(
-                           height: 4.h,
-                         ),
-
-                         CustomTextView(text: "testing")
-                       ],
-                     );
-                   },
-                 ),
-               ),
-             ],
-           ),
-
+            const StatusBarSection(),
           ],
         ),
       ),
--- lib/pages/homepage/homepageviewitem/statusbar_section.dart
+++ lib/pages/homepage/homepageviewitem/statusbar_section.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:untitled/widget/custom_text.dart';
@@ -8,7 +9,7 @@ class StatusBarSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200.h,
+      height: 250.h,
       padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 20.h),
       decoration: const BoxDecoration(
           gradient: LinearGradient(colors: [
@@ -70,7 +71,63 @@ class StatusBarSection extends StatelessWidget {
           Row(children: [
             CustomTextView(text: "5 Chapter",fontColor: Colors.grey.shade300,),SizedBox(width: 50.w,),
             CustomTextView(text: "Total 60:00",fontColor: Colors.grey.shade300,)
-          ],)
+          ],),
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: SizedBox(
+                  height: 300,
+                  child: ListView.separated(
+                    itemCount: 5,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    separatorBuilder: (BuildContext context, int index) => SizedBox(width: 10,),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade400,
+                            borderRadius: BorderRadius.circular(12)
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                                borderRadius: BorderRadius.only(topRight: Radius.circular(12),topLeft:Radius.circular(12)),
+                                child: CachedNetworkImage(
+                                  height: 200,
+                                  width: 200,
+                                  fit: BoxFit.fill,
+                                  imageUrl: "https://images.squarespace-cdn.com/content/v1/5a0d6e5dcd39c35d0c651639/1561378813145-POA9V823FOBYVV7RPH4K/5+Beginner+ESL+Lesson+Plans+You+Need+to+Try?format=500w",
+                                  errorWidget: (
+                                      context,
+                                      url,
+                                      error,
+                                      ) => const Icon(Icons.error), progressIndicatorBuilder: (context, url, downloadProgress,) =>
+                                    Center(
+                                      child: CircularProgressIndicator(
+                                          value: downloadProgress.progress),
+                                    ),
+                                )),SizedBox(height: 5,),
+                            CustomTextView(
+                              text: "Beginner Cource",
+                              fontColor: Colors.black,
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            SizedBox(
+                              height: 4.h,
+                            ),
+                            CustomTextView(text: "testing")
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
