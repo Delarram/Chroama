322dd5f3fd5ae8826cf4100b242bc5798231eb8d
Delarram <tunyanaing960@gmail.com>
2023-01-10 09:46:46 +0630

    some minor issue

--- lib/pages/homepage/home_page.dart
+++ lib/pages/homepage/home_page.dart
@@ -14,61 +14,66 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-           StatusBarSection(),
-           Padding(
-             padding: const EdgeInsets.symmetric(horizontal: 12),
-             child: Row(
-               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-               children: [
-                 CustomTextView(text: "Beginner Course",fontSize: 24.sp,fontWeight: FontWeight.w500,),
-                 CustomTextView(text: "See all",fontSize: 14.sp,fontWeight: FontWeight.w500,fontColor: Colors.grey.shade600,),
-               ],
-             ),
-           ),
-            SizedBox(
-              height: 300,
-              child: ListView.separated(
-                itemCount: 5,
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                separatorBuilder: (BuildContext context, int index) => SizedBox(width: 10,),
-                itemBuilder: (BuildContext context, int index) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: CachedNetworkImage(
-                            height: 200,
-                            width: 200,
-                            fit: BoxFit.fill,
-                            imageUrl: "https://images.squarespace-cdn.com/content/v1/5a0d6e5dcd39c35d0c651639/1561378813145-POA9V823FOBYVV7RPH4K/5+Beginner+ESL+Lesson+Plans+You+Need+to+Try?format=500w",
-                            errorWidget: (
-                                context,
-                                url,
-                                error,
-                                ) => const Icon(Icons.error), progressIndicatorBuilder: (context, url, downloadProgress,) =>
-                              Center(
-                                child: CircularProgressIndicator(
-                                    value: downloadProgress.progress),
-                              ),
-                          )),SizedBox(height: 5,),
-                       CustomTextView(
-                        text: "Beginner Cource",
-                        fontColor: Colors.black,
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.w600,
-                      ),
-                       SizedBox(
-                        height: 4.h,
-                      ),
+           Stack(
+             children: [
+               StatusBarSection(),
+               SizedBox(
+                 height: 300,
+                 child: ListView.separated(
+                   itemCount: 5,
+                   shrinkWrap: true,
+                   scrollDirection: Axis.horizontal,
+                   separatorBuilder: (BuildContext context, int index) => SizedBox(width: 10,),
+                   itemBuilder: (BuildContext context, int index) {
+                     return Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Padding(
+                           padding: const EdgeInsets.symmetric(horizontal: 12),
+                           child: Row(
+                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                             children: [
+                               CustomTextView(text: "Beginner Course",fontSize: 24.sp,fontWeight: FontWeight.w500,),
+                               CustomTextView(text: "See all",fontSize: 14.sp,fontWeight: FontWeight.w500,fontColor: Colors.grey.shade600,),
+                             ],
+                           ),
+                         ),
+                         ClipRRect(
+                             borderRadius: BorderRadius.circular(15),
+                             child: CachedNetworkImage(
+                               height: 200,
+                               width: 200,
+                               fit: BoxFit.fill,
+                               imageUrl: "https://images.squarespace-cdn.com/content/v1/5a0d6e5dcd39c35d0c651639/1561378813145-POA9V823FOBYVV7RPH4K/5+Beginner+ESL+Lesson+Plans+You+Need+to+Try?format=500w",
+                               errorWidget: (
+                                   context,
+                                   url,
+                                   error,
+                                   ) => const Icon(Icons.error), progressIndicatorBuilder: (context, url, downloadProgress,) =>
+                                 Center(
+                                   child: CircularProgressIndicator(
+                                       value: downloadProgress.progress),
+                                 ),
+                             )),SizedBox(height: 5,),
+                         CustomTextView(
+                           text: "Beginner Cource",
+                           fontColor: Colors.black,
+                           fontSize: 14.sp,
+                           fontWeight: FontWeight.w600,
+                         ),
+                         SizedBox(
+                           height: 4.h,
+                         ),
 
-                      CustomTextView(text: "testing")
-                    ],
-                  );
-                },
-              ),
-            )
+                         CustomTextView(text: "testing")
+                       ],
+                     );
+                   },
+                 ),
+               ),
+             ],
+           ),
+
           ],
         ),
       ),
